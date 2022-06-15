b1e7b099e76fd390626c6db75a3bba7fbbbf81bd
Your Name <you@example.com>
2022-06-15 00:17:33 -0300

    Se agregan componentes inicio contacto proyectos sobreMi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './Components/navbar/navbar.component'
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { InicioComponent } from './Components/inicio/inicio.component';
+import { ContactoComponent } from './Components/contacto/contacto.component';
+import { SobreMiComponent } from './Components/sobre-mi/sobre-mi.component';
+import { ProyectosComponent } from './Components/proyectos/proyectos.component'
 
 @NgModule({
   declarations: [
     AppComponent,
    NavbarComponent,
+   InicioComponent,
+   ContactoComponent,
+   SobreMiComponent,
+   ProyectosComponent,
   ],
   imports: [
     BrowserModule
