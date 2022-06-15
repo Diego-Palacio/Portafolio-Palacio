94696e45a861da26b9afa6a00e350b61411e6005
Your Name <you@example.com>
2022-06-15 00:37:50 -0300

    se agregan todas las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,2 +1,18 @@
 //Archivo a rutas
 
+import { RouterModule,Routes } from "@angular/router";
+import { ContactoComponent } from "./Components/contacto/contacto.component";
+import { InicioComponent } from "./Components/inicio/inicio.component";
+import { ProyectosComponent } from "./Components/proyectos/proyectos.component";
+import { SobreMiComponent } from "./Components/sobre-mi/sobre-mi.component";
+
+const app_routes: Routes=[
+    {path: 'inicio', component: InicioComponent},
+    {path: 'sobreMi', component: SobreMiComponent},
+    {path: 'proyectos', component: ProyectosComponent},
+    {path: 'contacto', component: ContactoComponent},
+    {path: '**', pathMatch: 'full', redirectTo:''}
+
+];
+
+export const app_routing = RouterModule.forRoot(app_routes);
